89535b58693cda79cf395353840a60e01b3ea18c
Mário Marcos <mariomms@gmail.com>
2020-01-14 09:23:53 -0300

    Delete lixo pasta antiga

--- CDFAD_local_alterar_email_para_envio_de_relatorios_publicacao.sql
+++
@@ -1,7 +0,0 @@
---São Paulo prod.
-
-/opt/approtinas/pro_21761_dwcorp/CONTROLM/relatorios/destinatarios
-
---Brasília prod.
-/pro_07643_bicondom_tra/MAINCONTROLM/relatorios/destinatarios  
-
--- Teradata/td_comando_para_iniciar_backup_na_40_teradata.sql
+++
@@ -1,8 +0,0 @@
-
-
-linux2
-na máquina ssh root@161.148.236.40
-/opt/teradata/tara/server/bin/taralaunch -j diario_full_prod_temp.arc -u administrator -p Admin_tara1
-
-
---tentar executar um copy para a máquina nova
