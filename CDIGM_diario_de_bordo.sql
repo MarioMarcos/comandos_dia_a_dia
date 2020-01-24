efc57ad57adbe430c8872a62c1060106904ab34e
MarioMarcos <59515079+MarioMarcos@users.noreply.github.com>
2020-01-24 14:37:44 -0300

    Update CDIGM_diario_de_bordo.sql

--- CDIGM_diario_de_bordo.sql
+++ CDIGM_diario_de_bordo.sql
@@ -12,6 +12,9 @@ FEVEREIRO/2020
 ------------------------------------------------------------------------
 JANEIRO/2020 - INICIEI O MEU DIÁRIO DE BORDO EM 2020 NA CDIGM
 -----------------------------------------------------------------------
+
+24/01/2020 - 
+24/01/2020 - Acompanhamento atividade para POC CYBERSPONSE com Ulisses Recife SS - [GOVI] NOTIFICAÇÃO DE ABERTURA DE SOLICITAÇÃO - SERPRO - Nº 2020SS/0000064425
 24/01/2020 - Leitura de documentação sobre servidor HUGO que será utilizado para trabalho com documentação estaleiro.
 24/01/2020 - Testes funcionais com Aurélio para encaminhar git projeto docs
 
