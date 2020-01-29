5917ae9f5cb58de26e18a8301745c55f555a1834
MarioMarcos <59515079+MarioMarcos@users.noreply.github.com>
2020-01-29 15:48:46 -0300

    Update CDIGM_diario_de_bordo.sql

--- CDIGM_diario_de_bordo.sql
+++ CDIGM_diario_de_bordo.sql
@@ -13,14 +13,18 @@ FEVEREIRO/2020
 JANEIRO/2020 - INICIEI O MEU DIÁRIO DE BORDO EM 2020 NA CDIGM
 -----------------------------------------------------------------------
 
+
+
+
+29/01/2020 - Continuação curso AWS para certificação.
 29/01/2020 - Curso AWS INNOVATE 
 
-27/01/2020 - Testes com Aurélio para trabalho com GTI no projeto documentação no Estaleiro
-27/01/2020 - Liguei para Negrini para saber previsão de data da máquina projeto SQL COLUNAR, o mesmo pediu para ligar em 20/02/2020
+28/01/2020 - Testes com Aurélio para trabalho com GTI no projeto documentação no Estaleiro
+28/01/2020 - Liguei para Negrini para saber previsão de data da máquina projeto SQL COLUNAR, o mesmo pediu para ligar em 20/02/2020
+
 27/01/2020 - Atendimento à André Magalhães para recuperar senha Teradata Admin 
 27/01/2020 - Liguei para Gustavo Monti, para falar sobre data de licitação dos softwares proprietários plataforma de IA
 27/01/2020 - Áudio - com Giovanni, Fernanda sobre dados de desativação do DATAPEN dos bancos no datalake
-
 
 
 24/01/2020 - Acompanhamento atividade para POC CYBERSPONSE com Ulisses Recife SS - [GOVI] NOTIFICAÇÃO DE ABERTURA DE SOLICITAÇÃO - SERPRO - Nº 2020SS/0000064425
@@ -76,57 +80,9 @@ no prazo - 2019SS/0000571227 - 02/07/2019 - pub dime desenv
 no prazo - 2019SS/0000571251 - 02/07/2019 - pub dime todos
 no prazo - 2019SS/0000564364 - 01/07/2019 - pub aduan bsb
 ------------------------------------------------------------------------
-JUNHO/2019
------------------------------------------------------------------------
-2019RDM/0000063372 - para mudança do PATH DO IRPF
-no prazo -2019RDM/0000048990 - 28/06/2019 - 
-no prazo - 2019SS/0000555399 - 28/06/2019 - dwpub tg
-no prazo - 2019SS/0000550419 - 27/06/2019 - dwpub tg
-no prazo - 2019SS/0000550574 - 27/06/2019 - dwput tg
-no prazo - 2019SS/0000539922 - 24/06/2019 - pub replica sigpgfn
-no prazo - 2019SS/0000543958 - 25/06/2019 - pub dwtg
-no prazo - 2019SS/0000526138 - 18/06/2019 - exec script
-no prazo - 2019SS/0000527274 - 18/06/2019 - pub sgpf
-no prazo - 2019SS/0000525580 - 18/06/2019 - pub pgfn
-no prazo - 2019SS/0000524417 - 18/06/2019 - info snsp
-no prazo - 2019SS/0000497557 - 11/06/2019
-no prazo - 2019SS/0000497648 - 11/06/2019
-no prazo - 2019SS/0000497618 - 11/06/2019
-no prazo - 2019SS/0000497592 - 12/06/2019
-no prazo - 2019SS/0000497582 - 12/06/2019
-no prazo - 2019SS/0000500964 - 12/06/2019
-no prazo - 2019SS/0000497356 - 12/06/2019
-no prazo - 2019SS/0000503582 - 13/06/2019
-no prazo - 2019SS/0000503474 - 13/06/2019
-no prazo - 2019SS/0000503462 - 13/06/2019
-no prazo - 2019SS/0000507893 - 14/06/2019
-no prazo - 2019SS/0000508037 - 14/06/2019
-no prazo - 2019SS/0000513957 - 14/06/2019
-no prazo - 2019SS/0000516261 - 14/06/2019
-no prazo - 2019SS/0000497356 - 10/06/2019 - pub CNPF
-no prazo - 2019SS/0000497582 - 10/06/2019 - pub dime des
-no prazo - 2019SS/0000497592 - 10/06/2019 - pub dime all toodos
-no prazo - 2019SS/0000497618 - 10/06/2019 - pub dime prod
-no prazo - 2019SS/0000497648 - 10/06/2019 - pub dime des
-no prazo - 2019SS/0000490596 - 07/06/2019 - pub DIME desen
-no prazo - 2019SS/0000490570 - 07/06/2019 - pub DIME prod 
-no prazo - 2019SS/0000490620 - 07/06/2019 - pub DIME todos
-no prazo - 2019RI/0000100072 - 08/06/2019 - inci área
-no prazo - 2019RDM/0000051758 - 10/06/2019 - big SGPJ
-no prazo - 2019SS/0000481845 - 05/06/2019 - PUB - PROC
-no prazo - 2019RI/0000099078 -  problema brasilia 
-no prazo - 2019SS/0000488255 - 06/06/2019 - PUB IRPF
-no prazo - 2019SS/0000488049 - 06/06/2019 - pub SSDSPU
-no prazo - 2019SS/0000487065 - 06/06/2019 - exec dwtg
-no prazo - 2019SS/0000486234 - 06/05/2019 - info SPED
-no prazo - 2019SS/0000482628 - 05/06/2019 - pub - cpnj
-no prazo - 2019SS/0000479569 - 04/06/2019 - pub pgfn
-no prazo - 2019SS/0000479461 - 04/06/2019 - exec script
-no prazo - 2019SS/0000478878 - 04/06/2019 - exec script
-no prazo - 2019SS/0000474725 - 04/06/2019 - pub dwtg
-no prazo - 2019SS/0000489754 são paulo produção problema
 
-SUNCF/CFTMA/CFTRB
+
+
 
 
 
