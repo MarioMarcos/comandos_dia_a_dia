5b5e883f456997720a81b6947746f1970c69680b
Mário Marcos <mariomms@gmail.com>
2020-05-28 08:52:37 -0300

    update files

---
+++ AWS_nuvem/Bootcamp_25_05_2020/aplicacao/s3-conf.php
@@ -0,0 +1,7 @@
+<?php
+$accessKey = 'AKIAU76L4NPJXYE4KP4S';
+$secretKey = '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';
+$region = 'us-east-1';
+$bucket = 'imagens.bootcamp.cloud.mario';
+$arqName =  'logo.jpg';
+?>
